5d773f95df6608df013d0bd8e30b27074e0b3c09
DurkaMateusz <mosca888@gmail.com>
2024-04-25 23:41:37 +0200

    added users MongoDB
    users options added: signup, login, logout, current. Authentication options added. Contacts are added to users

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
 require('dotenv').config();
 
 const { DB_HOST: urlDb } = process.env;
 
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
 
 const app = express()
 
@@ -18,6 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
